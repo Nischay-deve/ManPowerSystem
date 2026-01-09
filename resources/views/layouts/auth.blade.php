324899011f5addfb15999ffa12c4fc40ac5a4593
Core Techies <coretechiesofficial@gmail.com>
2026-01-09 18:31:46 +0530

    fixed

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<base href="{{ route('index') }}">
+		<!-- <base href="{{ route('index') }}"> -->
 
 		<!-- begin::GXON Meta Basic -->
 		<meta charset="utf-8">
