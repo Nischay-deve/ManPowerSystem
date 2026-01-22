6a7203a0beaff0301ce40eb920b5339466ac5786
Core Techies <coretechiesofficial@gmail.com>
2026-01-22 12:04:30 +0530

    logo fix

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -11,7 +11,7 @@
 			<img src="{{ asset('assets/images/auth/Workx_logo.png') }}"
 				alt="Xways Logo"
 				style="
-                max-height:100px;
+                max-height:77px;
                 width:auto;
                 object-fit:contain;
              ">
